cac66528e23b01aa34ce140a7f04dde255ab16d2
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 15:40:20 -0500

    refactor: hide cleanup

--- src/Helmet.tsx
+++ src/Helmet.tsx
@@ -1,6 +1,5 @@
 import Placeholder from '@sone-dao/sone-react-placeholder'
-import { sub } from '@sone-dao/sone-react-utils'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Account from './components/Account'
 import Hamburger from './components/Hamburger'
 import styles from './styles/Helmet.module.scss'
@@ -8,17 +7,6 @@ import styles from './styles/Helmet.module.scss'
 export interface IHelmetProps {}
 
 const Helmet: React.FC<IHelmetProps> = () => {
-  const [isHelmetHidden, setHelmetHidden] = useState<boolean>(false)
-
-  useEffect(() => {
-    sub('__TONE_EVENTS__', 'helmet.hide', (hidden: boolean) => {
-      console.log(`Helmet visibility set to ${hidden}`)
-      setHelmetHidden(hidden)
-    })
-  }, [])
-
-  if (isHelmetHidden) return <></>
-
   return (
     <div className={styles.helmet}>
       <Hamburger />
