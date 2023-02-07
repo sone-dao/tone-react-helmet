76ce5dfa31b7e620d0689137928a1dd0c280123d
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 14:20:56 -0500

    refactor: hide logic removal

--- src/Helmet.tsx
+++ src/Helmet.tsx
@@ -1,6 +1,5 @@
 import Placeholder from '@sone-dao/sone-react-placeholder'
-import { sub } from '@sone-dao/sone-react-utils'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Account from './components/Account'
 import Hamburger from './components/Hamburger'
 import styles from './styles/Helmet.module.scss'
@@ -8,18 +7,8 @@ import styles from './styles/Helmet.module.scss'
 export interface IHelmetProps {}
 
 const Helmet: React.FC<IHelmetProps> = () => {
-  const [isHidden, setHidden] = useState<boolean>(false)
-  const display = isHidden ? 'none' : 'flex'
-
-  useEffect(() => {
-    sub('__TONE_EVENTS__', 'helmet.hide', (hide: boolean) => {
-      console.log(`Helmet visibility set to: ${hide}`)
-      setHidden(hide)
-    })
-  }, [])
-
   return (
-    <div className={styles.helmet} style={{ display }}>
+    <div className={styles.helmet}>
       <Hamburger />
       <Placeholder display="Component: Search Box" width="300px" />
       <Account />
