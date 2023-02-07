0b1c6c4918f28cf91c5ef1a17f7ea75d5f3551d9
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 13:08:51 -0500

    feat: hide listener

--- src/Helmet.tsx
+++ src/Helmet.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Hamburger from './components/Hamburger'
-import Account from './components/Account'
-import styles from './styles/Helmet.module.scss'
 import Placeholder from '@sone-dao/sone-react-placeholder'
+import { sub } from '@sone-dao/sone-react-utils'
+import React, { useEffect, useState } from 'react'
+import Account from './components/Account'
+import Hamburger from './components/Hamburger'
+import styles from './styles/Helmet.module.scss'
 
 export interface IHelmetProps {}
 
 const Helmet: React.FC<IHelmetProps> = () => {
+  const [isHidden, setHidden] = useState<boolean>(false)
+  const display = isHidden ? 'none' : 'flex'
+
+  useEffect(() => {
+    sub('__TONE_EVENTS__', 'helmet.hide', (hide: boolean) => setHidden(hide))
+  }, [])
+
   return (
-    <div className={styles.helmet}>
+    <div className={styles.helmet} style={{ display }}>
       <Hamburger />
       <Placeholder display="Component: Search Box" width="300px" />
       <Account />
