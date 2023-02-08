f04cf8e4f70833acb1936649e4d2f8f514325e4e
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 21:23:25 -0500

    feat: responsive styling and search input

--- src/Helmet.tsx
+++ src/Helmet.tsx
@@ -1,7 +1,7 @@
-import Placeholder from '@sone-dao/sone-react-placeholder'
 import React from 'react'
 import Account from './components/Account'
 import Hamburger from './components/Hamburger'
+import SearchInput from './components/SearchInput'
 import styles from './Helmet.module.scss'
 
 export interface IHelmetProps {}
@@ -10,7 +10,7 @@ const Helmet: React.FC<IHelmetProps> = () => {
   return (
     <div className={styles.helmet}>
       <Hamburger />
-      <Placeholder display="Component: Search Box" width="300px" />
+      <SearchInput />
       <Account />
     </div>
   )
